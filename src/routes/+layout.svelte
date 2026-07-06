c1ce79d6075c919ea215f8ec3feafb29fc4a3a9c
AllanDwyre <allan.dwyre@gmail.com>
2026-07-06 20:16:35 +0200

    ✨ Main page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
-
+	import '../styles/main.scss';
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Averia+Sans+Libre:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="icon" href={favicon} />
 </svelte:head>
 
 {@render children()}
+
+<!-- Footer -->
+<!-- TODO Footer -->
---
+++ src/routes/cv/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Page du CV</h1>
